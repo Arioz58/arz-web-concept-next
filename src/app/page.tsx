2498414f30e0e3308f9beefa23ff8367cbc92e83
Arioz58 <berke.ciftci002200@gmail.com>
2025-02-02 21:37:38 +0100

    page.tsx fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import CustomCursor from "@/components/CustomCursor";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import About from "@/components/"
-import Services from "@/components/"
-import Footer from "@/components/"
+//import About from "@/components/"
+//import Services from "@/components/"
+//import Footer from "@/components/"
 
 export default function Home() {
   return (
