751b5582f202a8ee2bee95001836fa2f78a99b9f
Arioz58 <berke.ciftci002200@gmail.com>
2025-04-12 14:02:11 +0200

    ajout d'une meilleur section pour les services

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,47 +1,31 @@
 "use client";
-import React, { useState } from "react";
-import { motion } from "framer-motion";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import ServicesBg from "@/assets/services.png";
 import tempTFTM from "@/assets/TFTM.png";
 import tempINdex from "@/assets/INdex.png";
 import tempHouse from "@/assets/House.png";
-
-const images = [tempTFTM, tempINdex, tempHouse];
-
-const servicesVariants = {
-  hidden: {
-    opacity: 0,
-    filter: "blur(30px)",
-    scale: 0.9,
-    transition: {
-      duration: 0.3,
-    },
-  },
-  visible: {
-    opacity: 1,
-    filter: "blur(0px)",
-    scale: 1,
-    transition: {
-      duration: 0.5,
-      ease: "easeInOut",
-    },
-  },
-};
+import arzCharts from "@/assets/charts_arz.png";
+import { Search } from "lucide-react";
 
 export default function Services() {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  
-  React.useEffect(() => {
+  const [step, setStep] = useState(0);
+
+  useEffect(() => {
+    let currentStep = 0;
     const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 3000);
+      setStep((prevStep) => {
+        currentStep = (prevStep + 1) % 3;
+        return currentStep;
+      });
+    }, 2000);
 
     return () => clearInterval(interval);
   }, []);
+
   return (
     <section className="text-white mt-10" id="services">
-      <div className="w-full h-screen relative ">
+      <div className="w-full h-[1200px] relative ">
         <Image
           src={ServicesBg}
           alt="Services Background"
@@ -52,25 +36,100 @@ export default function Services() {
         />
         <div className="absolute inset-0 flex items-center justify-center">
           <div className="text-white z-10 p-2 ">
-            <h2 className="text-3xl lg:text-4xl sticky text-center mb-10">Services.</h2>
-            <div className="mx-auto my-2 backdrop-blur-md rounded-[60px] bg-gradient-to-r from-white/15 to-white/00 border border-white/10 p-2 md:p-4 md:rounded-[80px]">
-              <Image src="/assets/mobile_nav/logo-arz.svg" alt="Logo" className="opacity-30" width={70} height={70} />
-              <p className="text-center text-2xl">Site Web</p>
-              <motion.div
-                key={currentImageIndex}
-                initial="hidden"
-                animate="visible"
-                variants={servicesVariants}
-                className="px-10 py-4"
-              >
-                <Image
-                  src={images[currentImageIndex]}
-                  alt={`Service Image ${currentImageIndex + 1}`}
-                  width={500}
-                  height={300}
-                  className="object-contain"
-                />
-              </motion.div>
+            <h2 className="text-3xl lg:text-4xl sticky text-center mb-10">
+              Services.
+            </h2>
+            <div className="grid grid-cols-1 grid-rows-3 md:grid-cols-2 md:grid-rows-2 gap-2 m-2 max-w-xl">
+              <div className="md:col-span-2 backdrop-blur-md rounded-3xl bg-gradient-to-r from-white/15 to-white/00 border border-white/10 p-2 md:p-4 overflow-hidden">
+                <div className="flex flex-col mx-2">
+                  <h3 className="text-2xl">Site web</h3>
+                  <p className="text-md text-white/80">
+                    Tous nos projets sont faits avec{" "}
+                    <b className="text-white">soins</b>, ont des{" "}
+                    <b className="text-white">
+                      design professionnel et premium
+                    </b>
+                  </p>
+                  <div className="flex flex-row justify-center relative h-[200px] top-12">
+                    <Image
+                      src={tempTFTM}
+                      height={180}
+                      alt="House"
+                      className={`transition-all duration-1000 absolute ${
+                        step === 0
+                          ? "translate-x-0 z-0"
+                          : step === 1
+                          ? "-translate-x-20 -rotate-6 z-0"
+                          : "-translate-x-24 -rotate-12 z-0"
+                      }`}
+                    />
+                    <Image
+                      src={tempINdex}
+                      height={180}
+                      alt="House"
+                      className={`transition-all duration-1000 absolute ${
+                        step === 1
+                          ? "translate-x-20 rotate-6"
+                          : step === 0
+                          ? "translate-x-0 rotate-6 translate-y-full opacity-0 scale-75 blur-md"
+                          : "translate-x-24 rotate-12 "
+                      }`}
+                    />
+                    <Image
+                      src={tempHouse}
+                      height={180}
+                      alt="House"
+                      className={`transition-all duration-1000 absolute ${
+                        step === 2
+                          ? "translate-x-0"
+                          : step === 1
+                          ? "translate-x-0 rotate-6 translate-y-full opacity-0 scale-75 blur-md z-10"
+                          : "translate-y-full opacity-0 blur-md"
+                      }`}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="backdrop-blur-md rounded-3xl bg-gradient-to-r from-white/15 to-white/00 border border-white/10 p-2 md:p-4 overflow-hidden">
+                <div className="flex flex-col mx-2">
+                  <h3 className="text-2xl">SEO</h3>
+                  <p className="text-md text-white/80 z-10">
+                    <b className="text-white">Investissez pour votre succès</b>
+                    —Nous ne faisons pas seulement des sites, nous réfléchissons
+                    a votre succès.
+                  </p>
+                  <div className="flex items-center justify-center mt-5">
+                    <div className="bg-[#7B72FF]/30 border-[#7B72FF] border-2 rounded-full h-32 w-32 z-0 absolute animate-ping">
+                      <div className="bg-[#7B72FF] w-full h-full rounded-full blur-xl"></div>
+                    </div>
+                    <div className="bg-white flex items-center justify-center rounded-full h-32 w-32 z-10 ">
+                      <div className="bg-radial from-white via-white to-[#7B72FF] border-[#7B72FF] border-2 flex items-center justify-center rounded-full h-28 w-28">
+                        <Search className="h-14 w-14 text-black" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="backdrop-blur-md rounded-3xl bg-gradient-to-r from-white/15 to-white/00 border border-white/10 p-2 md:p-4 overflow-hidden">
+                <div className="flex flex-col mx-2">
+                  <h3 className="text-2xl">Branding</h3>
+                  <p className="text-md text-white/80">
+                    Nous créons le meilleur moyen de vous{" "}
+                    <b className="text-white">démarquez visuellement.</b>
+                  </p>
+                  <div className="flex justify-center">
+                    <Image
+                      src={arzCharts}
+                      alt="House"
+                      height={200}
+                      className="absolute -rotate-6 bottom-0"
+                      style={{
+                        scale: "1.4",
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundImage: {
+        'radial' : "radial-gradient(var(--tw-gradient-stops))",
+      },
     },
   },
   plugins: [],
